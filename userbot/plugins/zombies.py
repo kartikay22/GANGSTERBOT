5b5f268133f32c6424e0bb622ae8685966f801a4
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-08-28 17:59:46 +0530

    Update zombies.py

--- userbot/plugins/zombies.py
+++ userbot/plugins/zombies.py
@@ -5,7 +5,7 @@
 """Cmd= `.zombie`
 Usage: Searches for deleted accounts in a groups and channels.
 Use .zombies clean to remove deleted accounts from the groups and channels.
-\nPorted by ©[NIKITA](t.me/kirito6969) and ©[EYEPATCH](t.me/NeoMatrix90)"""
+\nPorted by ©[DEAR COMRADE](t.me/THE_DEAR_COMRADE)"""
 
 from telethon import events
 from userbot.utils import admin_cmd
